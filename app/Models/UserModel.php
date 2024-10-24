870beb5bfa689462eb8951bdbdf29cb301420bb0
Arif <112569342+MuhammadArifRizkiPohan@users.noreply.github.com>
2024-10-25 06:49:58 +0700

    database model

---
+++ app/Models/UserModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    use HasFactory;
+
+    // Tentukan tabel yang akan digunakan
+    protected $table = 'user';  // Menggunakan tabel 'users' di database
+
+    // Guarded digunakan untuk menghindari mass assignment pada kolom tertentu
+    protected $guarded = ['id'];
+
+    // Definisikan relasi belongsTo ke model Kelas
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class, 'kelas_id');
+    }
+}
